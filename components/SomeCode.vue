b7081bb8163d7c83525b0bdeac291e7716fdce79
hammas-19 <hammasmasood@gmail.com>
2023-10-24 17:18:45 -0700

    style: Designed copying code

---
+++ components/SomeCode.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="bg-[#f5f2fc]">
+    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+      <div class="sm:flex sm:items-center sm:justify-between">
+        <p class="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+          Build with ❤️ by Dev 🔥 for Devs 🔥
+        </p>
+        <div class="flex justify-center text-teal-600 sm:justify-start">
+          <img src="/Tail_widget_logo.png" class="w-10 h-fit" alt="TailWidget">
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
