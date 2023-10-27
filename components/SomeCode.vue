e928679acc6f4f4e4701f2035da7a50e25d26b1f
hammas-19 <hammasmasood@gmail.com>
2023-10-26 17:25:52 -0700

    chore: some streak code

--- components/SomeCode.vue
+++ components/SomeCode.vue
@@ -10,5 +10,35 @@
         </div>
       </div>
     </div>
+    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 hidden">
+      <div class="sm:flex sm:items-center sm:justify-between">
+        <p class="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+          Build with ❤️ by Dev 🔥 for Devs 🔥
+        </p>
+        <div class="flex justify-center text-teal-600 sm:justify-start">
+          <img src="/Tail_widget_logo.png" class="w-10 h-fit" alt="TailWidget">
+        </div>
+      </div>
+    </div>
+    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 hidden">
+      <div class="sm:flex sm:items-center sm:justify-between">
+        <p class="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+          Build with ❤️ by Dev 🔥 for Devs 🔥
+        </p>
+        <div class="flex justify-center text-teal-600 sm:justify-start">
+          <img src="/Tail_widget_logo.png" class="w-10 h-fit" alt="TailWidget">
+        </div>
+      </div>
+    </div>
+    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 hidden">
+      <div class="sm:flex sm:items-center sm:justify-between">
+        <p class="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+          Build with ❤️ by Dev 🔥 for Devs 🔥
+        </p>
+        <div class="flex justify-center text-teal-600 sm:justify-start">
+          <img src="/Tail_widget_logo.png" class="w-10 h-fit" alt="TailWidget">
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
